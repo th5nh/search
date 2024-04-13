0cac168d0c7c786ef10ae895c37545fe70fd0519
th5nh <vuthanh511203@gmail.com>
2024-04-13 14:57:29 +0700

    A little bit edit

--- controller.py
+++ controller.py
@@ -4,4 +4,4 @@ from const import *
 
 def Initialize (myMatrix :Matrix) : 
     COLS, ROWS = myMatrix.width+1, myMatrix.height+1
-    RES = WIDTH, HEIGHT = 800+2*BOUND + (ROWS-1)*(A1), 800+2*BOUND + (COLS-1)*(A1)
+    #RES = WIDTH, HEIGHT = 800+2*BOUND + (ROWS-1)*(A1), 800+2*BOUND + (COLS-1)*(A1)
